75879263aa69535cad434e13c7ea6f09362bb05b
Batiscafff <olegpley2424@outlook.com>
2025-07-30 12:02:58 +0000

    CI debug: DNS port change

--- tests/test_dns.py
+++ tests/test_dns.py
@@ -1,25 +1,36 @@
-import dns.resolver
+import dns.message
+import dns.query
 import dns.flags
+import dns.rdatatype
 import pytest
 
 dns_base_ip = "127.0.0.1"
+dns_base_port = 5335
 
-def query_custom_dns(domain, record_type, custom_dns_ip):
-    resolver = dns.resolver.Resolver()
-    resolver.nameservers = [custom_dns_ip]
+def query_dns_over_udp(domain, record_type, ip, port):
+    query = dns.message.make_query(domain, record_type, want_dnssec=True)
     try:
-        answers = resolver.resolve(domain, record_type)
-        return answers
-    except dns.resolver.NoAnswer:
-        assert False, f"No {record_type} record found for {domain} on {custom_dns_ip}"
+        response = dns.query.udp(query, ip, port=port, timeout=2)
+        return response
     except Exception as e:
-        assert False, f"Error querying {record_type} record for {domain}: {e}"
+        pytest.fail(f"DNS query to {ip}:{port} failed: {e}")
 
-def test_dns_resolver():
-    record_type_to_query = "A"
-    domain_to_query = "google.com"
-    results = query_custom_dns(domain_to_query, record_type_to_query, dns_base_ip)
-    if results:
-        for rdata in results:
-            assert rdata.to_text(), f"The answer cannot be recognized"
+def test_dns_resolver_on_custom_port():
+    domain = "google.com"
+    record_type = "A"
+
+    response = query_dns_over_udp(domain, record_type, dns_base_ip, dns_base_port)
+
+    answers = [rr.to_text() for rrset in response.answer for rr in rrset]
+    assert answers, f"No {record_type} records found for {domain}"
+    ad_flag = bool(response.flags & dns.flags.AD)
+    print("AD flag set:", ad_flag)
+
+def test_dnssec():
+    domain = "dnssec-failed.org"
+    record_type = "A"
+
+    response = query_dns_over_udp(domain, record_type, dns_base_ip, dns_base_port)
+
+    assert response.rcode() == 2, f"The {domain} was resolved by {dns_base_ip}:{dns_base_port}"
 
